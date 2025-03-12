df2a0e2e6a7e4f7ec3a49e72dff5601199f2dc8f
SamantaRAMOS <148590713+SamantaRAMOS@users.noreply.github.com>
2025-03-12 11:42:40 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc"; // Usa la versión correcta del plugin
 
 export default defineConfig({
   plugins: [react()],
-  base: "/c24-22-m-webapp/", // Asegúrate de poner el nombre del repositorio de GitHub
+  base: "/c24-22-m-webapp/", // Asegúrate de usar el nombre correcto del repositorio
 });
+
